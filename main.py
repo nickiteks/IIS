5228b98bd6d8458bf9601a1850367e777b89efc9
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-11-03 21:28:19 +0400

    lab5

--- main.py
+++ main.py
@@ -1,33 +1,22 @@
-import pandas as p
+from scipy.cluster.hierarchy import linkage, dendrogram
+import matplotlib.pyplot as plt
+import pandas as pd
 
-data = p.read_csv('titanic.csv')
+seeds_df = pd.read_csv("http://qps.ru/jNZUT")
 
-''' --Keys--
-0 - PassengerId
-1 - Survived
-2 - Pclass
-3 - Name
-4 - Sex
-5 - Age
-6 - SibSp
-7 - Ticket
-8 - Fare
-9 - Cabin
-10 - Embarked
-'''
-keys = data.keys()
+# Удалиние информации об образцах зерна
+varieties = list(seeds_df.pop('grain_variety'))
+
+samples = seeds_df.values
 
 
-def count_survived():
-    survived = 0
+hierarchical_clustering = linkage(samples, method='complete')
 
-    for i in data[keys[1]]:
-        if i == 1:
-            survived = survived + 1
+# Дендограмма
+dendrogram(hierarchical_clustering,
+           labels=varieties,
+           leaf_rotation=90,
+           leaf_font_size=6,
+           )
 
-    return survived
-
-
-result = round(count_survived() / len(data[keys[1]]) * 100, 2)
-
-print(result)
+plt.show()
