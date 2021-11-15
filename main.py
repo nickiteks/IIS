d4078619ece70ca44853bde8ddb657e6d11bf9f5
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-11-15 21:14:26 +0400

    lab6

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
-from scipy.cluster.hierarchy import linkage, dendrogram
-import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
-seeds_df = pd.read_csv("http://qps.ru/jNZUT")
+# Даныне фильмов
+metadata = pd.read_csv('movies_metadata.csv', low_memory=False)
 
-# Удалиние информации об образцах зерна
-varieties = list(seeds_df.pop('grain_variety'))
+# Формула для расчета рейтинга WR = ((v/(v+m)) * R )+( (m/(v+m)) * C )
+# v = vote_count
+# R = vote_average
+# m - Установитьь процентаж
 
-samples = seeds_df.values
+C = metadata['vote_average'].mean()
+m = metadata['vote_count'].quantile(0.90)
 
 
-hierarchical_clustering = linkage(samples, method='complete')
+def weighted_rating(x, m=m, C=C):
+    v = x['vote_count']
+    R = x['vote_average']
+    # Calculation based on the IMDB formula
+    return ((v / (v + m)) * R) + ((m / (v + m)) * C)
 
-# Дендограмма
-dendrogram(hierarchical_clustering,
-           labels=varieties,
-           leaf_rotation=90,
-           leaf_font_size=6,
-           )
 
-plt.show()
+# Новый датаФрейм для того что бы не испортить стартоввый
+q_movies = metadata.copy().loc[metadata['vote_count'] >= m]
+q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
+q_movies = q_movies.sort_values('score', ascending=False)
+
+tfidf = TfidfVectorizer(stop_words='english')
+
+metadata['overview'] = metadata['overview'].fillna('')
+
+tfidf_matrix = tfidf.fit_transform(metadata['overview'])
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
+
+
+def get_recommendations(title, cosine_sim=cosine_sim):
+    idx = indices[title]
+
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    sim_scores = sim_scores[1:11]
+
+    movie_indices = [i[0] for i in sim_scores]
+
+    return metadata['title'].iloc[movie_indices]
+
+
+print(get_recommendations('Spider-Man'))
