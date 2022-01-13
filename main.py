652d2dca51186402707a0ded406d35c31e4eaa31
nickiteks <55086354+nickiteks@users.noreply.github.com>
2022-01-13 18:45:57 +0400

    exam

--- main.py
+++ main.py
@@ -1,53 +1,41 @@
-from sklearn.feature_selection import RFE
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
+import pandas as pd
+import random as r
+import names
+from matplotlib import pyplot as plt
+
+list_eyes_color = ['Красные', 'Фиолетовые', 'Черные']
+
+data = pd.DataFrame({'Длина носа': [r.randint(1, 20) for i in range(100)],
+                     'Размер ноги': [r.randint(1, 30) for i in range(100)],
+                     'Имя': [names.get_full_name() for i in range(100)],
+                     'Пиво буиш?': [r.choice([True, False]) for i in range(100)],
+                     'Цвет глаз': [r.choice(list_eyes_color) for i in range(100)]})
+
+data.to_csv('file.csv')
+
+a = data['Длина носа'].min()
+b = data['Длина носа'].max()
+c = data['Длина носа'].mean()
+d = data['Длина носа'].median()
+
+print(a, b, c, d)
 
 
-def rank_to_dict(ranks, names):
-    ranks = np.abs(ranks)
-    minmax = MinMaxScaler()
-    ranks = minmax.fit_transform(np.array(ranks).reshape(14, 1)).ravel()
-    ranks = map(lambda x: round(x, 2), ranks)
-    return dict(zip(names, ranks))
+def min_cl():
+    min = data['Длина носа'][0]
+    for i in range(len(data['Длина носа'])):
+        if data['Длина носа'][i] < min:
+            min = data['Длина носа'][i]
+    return min
 
 
-def sort_data(dict1):
-    sorted_values = sorted(dict1.values())  # Sort the values
-    sorted_dict = {}
+print(min_cl())
 
-    for i in sorted_values:
-        for k in dict1.keys():
-            if dict1[k] == i:
-                sorted_dict[k] = dict1[k]
-                break
-    return sorted_dict
-
-
-np.random.seed(0)
-size = 750
-X = np.random.uniform(0, 1, (size, 14))
-# Задаем функцию-выход: регрессионную проблему Фридмана
-Y = (10 * np.sin(np.pi * X[:, 0] * X[:, 1]) + 20 * (X[:, 2] - .5) ** 2 +
-     10 * X[:, 3] + 5 * X[:, 4] ** 5 + np.random.normal(0, 1))
-# Добавляем зависимость признаков
-X[:, 10:] = X[:, :4] + np.random.normal(0, .025, (size, 4))
-
-lr = LinearRegression()
-lr.fit(X, Y)
-
-rfe = RFE(lr)
-rfe.fit(X, Y)
-
-regr_tree = RandomForestRegressor(random_state=0)
-regr_tree.fit(X, Y)
-
-names = ["x%s" % i for i in range(1, 15)]
-rank = [rank_to_dict(lr.coef_, names),
-        rank_to_dict(regr_tree.feature_importances_, names)]
-
-for i in range(len(rank)):
-    print(sort_data(rank[i]))
-
-print(rank_to_dict(rfe.support_, names))
+fig, ax = plt.subplots()
+ax.set_title('Длина носа')
+ax.set_xlabel('Люди')
+ax.set_ylabel('Длина')
+plt.plot(data['Длина носа'])
+plt.plot(data['Размер ноги'])
+plt.grid('on')
+plt.show()
