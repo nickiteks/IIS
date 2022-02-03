19fee0b492edcadd77fca2042c97002f6e8203a5
nickiteks <55086354+nickiteks@users.noreply.github.com>
2022-02-03 22:20:52 +0400

    fix bugs

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from operator import itemgetter
+
+import joblib
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+
+
+def save_file(model, filename):
+    joblib.dump(model, filename)
+
+
+def load_file(filename):
+    return joblib.load(filename)
+
 
 # Данные фильмов
 metadata = pd.read_csv('movies_metadata.csv', low_memory=False)
@@ -10,9 +22,12 @@ tfidf = TfidfVectorizer(stop_words='english')
 metadata['overview'] = metadata['overview'].fillna('')
 
 tfidf_matrix = tfidf.fit_transform(metadata['overview'])
+
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-indices = pd.Series(metadata.index, index=metadata['title']).drop_duplicates()
+save_file(cosine_sim,'model.pkl')
+
+indices = metadata['title'].drop_duplicates().reset_index().set_index('title')['index']
 
 
 # высчитивание похожих фильмов на основе описания фильмов
@@ -21,7 +36,7 @@ def get_recommendations(title, cosine_sim=cosine_sim):
 
     sim_scores = list(enumerate(cosine_sim[idx]))
 
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sorted(sim_scores, key=itemgetter(1), reverse=True)
 
     sim_scores = sim_scores[1:11]
 
@@ -30,4 +45,4 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     return metadata['title'].iloc[movie_indices]
 
 
-print(get_recommendations('Spider-Man'))
+print(get_recommendations('The Avengers'))
